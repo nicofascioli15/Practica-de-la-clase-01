c675228cdf35bfde241f92e216a4e28b2a42dfaf
nicofascioli15 <121265494+nicofascioli15@users.noreply.github.com>
2023-03-10 22:50:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nacimiento = prompt ("tu año de nacimiento")
+let anio = prompt ("en que año estamos?")
+
+let resultado = parseInt(anio) - parseInt (nacimiento)
+
+alert ("hola tenes" + " " + resultado + " " + "años")
+
+
